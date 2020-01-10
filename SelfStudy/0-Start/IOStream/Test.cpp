c17d32a051b1bb25502f25857336aadf06d6b932
wMiu <miu0619@163.com>
2020-01-10 14:03:33 +0800

    IOStream . Test 源文件与编译

---
+++ SelfStudy/0-Start/IOStream/Test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	/*
+		练习1.4 打印 使用加法运算符 '+' 将两个数相加，再试试乘法 '*'。
+	*/
+	int num1 = 3, num2 = 5;
+	cout << "3 + 5 = " << num1 + num2 << endl;
+	cout << "3 * 5 = " << num1 * num2 << endl;
+}
