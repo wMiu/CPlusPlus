879f3c595655eb7d91761229e87cb8553a36919a
wMiu <miu0619@163.com>
2020-02-10 16:56:41 +0800

    空指针的补充

--- SelfStudy/1-CppBasics/4-CompoundType/Pointer/pointer.cpp
+++ SelfStudy/1-CppBasics/4-CompoundType/Pointer/pointer.cpp
@@ -8,7 +8,18 @@ int main(){
     cout << "num的值：" << num << "\n" 
          << "&num: " << &num << "\n"
          << "ptr_num: " << ptr_num << "\n"
-         << "*ptr_num: " << *ptr_num << endl;  
+         << "*ptr_num: " << *ptr_num << endl;
+		 
+	int i = 42;
+	int *p1 = 0;
+	if (p1) {
+		cout << "p1" << endl;
+	}
+	int *p2 = &i;
+	if (p2) {
+		cout << "p2" << endl;
+	}
+	// 任何非0指针都为True
 
     return 0;
 }
