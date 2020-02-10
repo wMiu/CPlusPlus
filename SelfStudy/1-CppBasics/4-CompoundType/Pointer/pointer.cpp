06e56766f31a6231b1a642884e42fe58f027ea42
wMiu <miu0619@163.com>
2020-02-10 17:02:32 +0800

    Void*

--- SelfStudy/1-CppBasics/4-CompoundType/Pointer/pointer.cpp
+++ SelfStudy/1-CppBasics/4-CompoundType/Pointer/pointer.cpp
@@ -20,6 +20,13 @@ int main(){
 		cout << "p2" << endl;
 	}
 	// 任何非0指针都为True
-
+	
+	int ii = 1;
+	double dd = 1.01; 
+	void *pp1 = &ii;
+	cout << pp1 << endl;
+	pp1 = &dd;
+	cout << pp1 << endl;
+	
     return 0;
 }
